5142c7feb0b00a6a72c251d8ed589a66408803b3
miyigit <muradsf@gmail.com>
2017-12-28 07:12:29 -0800

    Initialize Repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+
+  get 'users/new'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
 end
